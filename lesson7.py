d5b6cf4b84dd184f9a5b0cc5b61bec9353fb7a20
bekkkks <ewefe811@gmail.com>
2022-12-27 20:40:45 +0600

    'less7'

--- lesson7.py
+++ lesson7.py
@@ -21,8 +21,8 @@ print('искомый элемент', search_for, 'найден под инде
 
 # сортировка выборкой
 # o(n2)
-nums = [2, 3, 1, 3, 1, 5, 7, 85, 5, 3, 44, 9, 9]
-print(sorted(nums))
+nums = [2, 3, 1, 3, 1, 5, 7, 85, 5, 2, 2, 5, 6, 7, 0, 11, 33, 3, 44, 9, 9]
+
 print('было', nums)
 
 for i in range(len(nums)):
@@ -32,8 +32,75 @@ for i in range(len(nums)):
         if nums[j] < nums[lowest]:
             lowest = j
 
-    nums[i],nums[lowest]=nums[lowest],nums[i]
-n=1
-m=2
-n,m=m,n
-print('стало',nums)
+    nums[i], nums[lowest] = nums[lowest], nums[i]
+n = 1
+m = 2
+n, m = m, n
+print('стало', nums)
+
+# o(log n)
+# бинарный поиск
+
+ma = sorted(nums)
+print(ma)
+
+nu = 0
+
+lowest = 0
+highest = len(ma) - 1
+index = None
+
+while (lowest <= highest) and (index is None):
+    # повторять пока не найдем
+    mid = (lowest + highest) // 2
+    if ma[mid] == nu:
+        index = mid
+    else:
+        if nu < ma[mid]:
+            # ищем по левой части списка
+            highest = mid - 1
+        else:
+            # по правой
+            lowest = mid + 1
+
+print('искомый элемент', nu, 'находиться под индексом', index)
+
+
+# алгоритм Евклида - нахождение нод
+
+def nod(a, b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return b + a
+
+
+print('НОД чисел 70 120=', nod(70, 120))
+
+from math import gcd
+
+print(gcd(70, 120))
+
+# поворот строки
+
+some_string = 'я не сегодня заболел!'
+
+
+def reversee(s):
+    chars = list(s)
+
+    for i in range(len(s) // 2):
+        #         до середины
+        temp = chars[i]
+        chars[i] = chars[len(s) - i - 1]
+        chars[len(s) - i - 1] = temp
+
+    return ''.join(chars)
+
+print(some_string)
+print(reversee(some_string))
+# print(some_string[::-1])
+
+
